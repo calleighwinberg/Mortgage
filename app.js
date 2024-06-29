e76c9ff882d9f7dcdc769c8f59374f34f53e90b0
calleighwinberg <calleighwinberg@gmail.com>
2024-06-29 07:32:19 -0700

    styling navbar and adding functionality

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/', (req, res) => {
@@ -67,6 +68,10 @@ app.delete('/tcas/:id', async (req,res) => {
     const { id } = req.params;
     await TCA.findByIdAndDelete(id)
     res.redirect('/tcas');
+})
+
+app.post('/tcas/:id/scenarios', async(req,res) => {
+    
 })
 
 
--- models/scenario.js
+++ models/scenario.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const scenario = new Schema({
-    name: String,
     price: Number,
     downPayment: Number,
     rate: Number,
-    term: Number
+    term: Number,
+    aprCosts: Number,
+    points: Number,
+    prepaidInterest: Number,
+    escrowFees: Number, 
+    noAPRcosts: Number, 
+    hoa: Number, 
+    hazIns: Number,
+    taxes: Number,
+    pmi: Number
 })
